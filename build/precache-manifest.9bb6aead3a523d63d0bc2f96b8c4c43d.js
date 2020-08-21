1f1563dde4ee50c1a3f54f31caa4941cef528801
mahendermuppa <mahendermuppa@gmail.com>
2020-08-21 15:06:05 +0530

    Add files via upload

---
+++ build/precache-manifest.9bb6aead3a523d63d0bc2f96b8c4c43d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a167c26f16220985e4bfe693a28393d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865c47a16404e8c40214",
+    "url": "/static/css/main.dba4e11b.chunk.css"
+  },
+  {
+    "revision": "16de9b42053c9a92caa0",
+    "url": "/static/js/2.dc5ee991.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.dc5ee991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865c47a16404e8c40214",
+    "url": "/static/js/main.f3d0e72f.chunk.js"
+  },
+  {
+    "revision": "426f24d4e100f6e22754",
+    "url": "/static/js/runtime-main.6637dc6b.js"
+  }
+]);
